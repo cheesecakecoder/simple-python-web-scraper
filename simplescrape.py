f5170878dbc3c688b6ef3c437d751ffe6c148f8e
Tom Chmielarz <tom.chmielarz@gmail.com>
2018-05-11 14:16:44 -0400

    Create simplescrape.py

---
+++ simplescrape.py
@@ -0,0 +1,15 @@
+import requests
+
+url = "http://webscrapingscripts.com/"
+extract_from = "<title>";
+extract_to = "</title>";
+
+page = requests.get(url)
+
+if page.status_code == 200:
+    print page.content
+    print "\r\n"
+    title = page.content[page.content.find(extract_from)+len(extract_from):page.content.find(extract_to)]
+    print "The scraped title is: " + title + "\r\n"
+else:
+    print "Error: Page did not return expected 200 OK\r\n"
